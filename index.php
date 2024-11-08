02d2b13b1c5d290cfeaa8d2939bbe7484a93c15a
anaGianno <blcoronado2004@gmail.com>
2024-11-08 15:13:43 +1300

    add basic interface to index

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
     <link rel="stylesheet" href = "style.css">
   </head>
   <body>
-
+    <div class = "header">header</div>
+    <div class = "content">
+        <p class = 'title'>Sanaple</p>
+        <input class = "url" type="text" id="url" name="url"><br>
+        <button>Search</button> 
+    </div>
     <script src = "script.js"></script>
   </body>
 </html>
